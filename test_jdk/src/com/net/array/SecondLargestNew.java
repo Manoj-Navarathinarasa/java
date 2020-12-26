d1f0cbb3a1a5d8d91bcfca31f7194b9294e50eb0
Manojnavarathinarasa <navarathinarasamanoj@gmail.com>
2020-12-27 00:05:06 +0530

    3rd Work

--- test_jdk/src/com/net/array/SecondLargestNew.java
+++ test_jdk/src/com/net/array/SecondLargestNew.java
@@ -1,2 +1,29 @@
-package com.net.array;public class SecondLargestNew {
+package com.net.array;
+
+public class SecondLargestNew {
+    public static void main(String args[])
+    {
+        int[] array = {69,12,76,26,82,3,89,96,94,112};
+
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+
+
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] > highest)
+            {
+                // ...shift the current highest number to second highest
+                secondHighest = highest;
+                // ...and set the new highest.
+                highest = array[i];
+            } else if (array[i] > secondHighest)
+            {
+                // Just replace the second highest
+                secondHighest = array[i];
+            }
+        }
+        System.out.println("second largest is "+secondHighest );
+        System.out.println("largest is "+ highest);
+    }
 }
